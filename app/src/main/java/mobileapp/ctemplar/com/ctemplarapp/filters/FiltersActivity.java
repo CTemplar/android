92d86d3c5f02da7d69fd35b4183bdbff582fd2f8
alexdwork2018 <alex.d.work.2018@gmail.com>
2019-08-08 21:03:51 +0300

    user filter add, edit, swipe delete

--- app/src/main/java/mobileapp/ctemplar/com/ctemplarapp/filters/FiltersActivity.java
+++ app/src/main/java/mobileapp/ctemplar/com/ctemplarapp/filters/FiltersActivity.java
@@ -2,26 +2,36 @@ package mobileapp.ctemplar.com.ctemplarapp.filters;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import mobileapp.ctemplar.com.ctemplarapp.BaseActivity;
 import mobileapp.ctemplar.com.ctemplarapp.R;
+import mobileapp.ctemplar.com.ctemplarapp.main.RecycleDeleteSwiper;
+import mobileapp.ctemplar.com.ctemplarapp.net.ResponseStatus;
 import mobileapp.ctemplar.com.ctemplarapp.net.response.Filters.FilterResult;
 import mobileapp.ctemplar.com.ctemplarapp.net.response.Filters.FiltersResponse;
 
 public class FiltersActivity extends BaseActivity {
     private FiltersViewModel filtersModel;
+    private FiltersAdapter filtersAdapter;
 
     @BindView(R.id.activity_filters_recycler_view)
     RecyclerView filtersRecyclerView;
@@ -57,7 +67,22 @@ public class FiltersActivity extends BaseActivity {
                 handleFiltersResponse(filtersResponse);
             }
         });
+        filtersModel.getDeleteFilterResponseStatus().observe(this, new Observer<ResponseStatus>() {
+            @Override
+            public void onChanged(@Nullable ResponseStatus responseStatus) {
+                handleFilterDeletingStatus(responseStatus);
+            }
+        });
         getFilters();
+        setupSwiperForFilterList();
+    }
+
+    private void handleFilterDeletingStatus(ResponseStatus responseStatus) {
+        if (responseStatus == null || responseStatus == ResponseStatus.RESPONSE_ERROR) {
+            Toast.makeText(getApplicationContext(), getString(R.string.txt_filter_not_deleted), Toast.LENGTH_SHORT).show();
+        } else if (responseStatus == ResponseStatus.RESPONSE_COMPLETE) {
+            Toast.makeText(getApplicationContext(), getString(R.string.txt_filter_deleted), Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void handleFiltersResponse(FiltersResponse filtersResponse) {
@@ -65,7 +90,7 @@ public class FiltersActivity extends BaseActivity {
             return;
         }
         List<FilterResult> filterList = filtersResponse.getFilterResultList();
-        FiltersAdapter filtersAdapter = new FiltersAdapter(filterList);
+        filtersAdapter = new FiltersAdapter(filterList);
         filtersRecyclerView.setAdapter(filtersAdapter);
     }
 
@@ -73,10 +98,52 @@ public class FiltersActivity extends BaseActivity {
         filtersModel.getFilters();
     }
 
+    private void setupSwiperForFilterList() {
+        RecycleDeleteSwiper swipeHandler = new RecycleDeleteSwiper(this) {
+            @Override
+            public void onSwiped(final @NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                final FiltersAdapter adapter = filtersAdapter;
+                if (adapter == null) {
+                    return;
+                }
+
+                final int deletedIndex = viewHolder.getAdapterPosition();
+                final FilterResult deletedFilter = adapter.removeAt(deletedIndex);
+                new AlertDialog.Builder(FiltersActivity.this)
+                        .setTitle(getString(R.string.txt_delete_filter_quest_title))
+                        .setMessage(getString(R.string.txt_delete_filter_quest_message))
+                        .setPositiveButton(getString(R.string.btn_contact_delete), new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        long filterId = deletedFilter.getId();
+                                        filtersModel.deleteFilter(filterId);
+                                    }
+                                }
+                        )
+                        .setNeutralButton(getString(R.string.btn_cancel), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                adapter.restoreItem(deletedIndex, deletedFilter);
+                            }
+                        })
+                        .show();
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(swipeHandler);
+        itemTouchHelper.attachToRecyclerView(filtersRecyclerView);
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         getFilters();
+    }
+
+    @OnClick(R.id.filters_footer_btn)
+    public void onClickAddFilter() {
+        Intent addFilterIntent = new Intent(this, AddFilterActivity.class);
+        startActivity(addFilterIntent);
     }
 
     @Override
--- app/src/main/java/mobileapp/ctemplar/com/ctemplarapp/filters/FiltersAdapter.java
+++ app/src/main/java/mobileapp/ctemplar/com/ctemplarapp/filters/FiltersAdapter.java
@@ -1,5 +1,7 @@
 package mobileapp.ctemplar.com.ctemplarapp.filters;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -27,13 +29,41 @@ public class FiltersAdapter extends RecyclerView.Adapter<FiltersViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull FiltersViewHolder holder, int position) {
-        FilterResult filterResult = filterList.get(position);
+        final Context context = holder.root.getContext();
+        final FilterResult filterResult = filterList.get(position);
         String name = filterResult.getName();
         holder.name.setText(name);
+        holder.root.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent editFilter = new Intent(context, EditFilterActivity.class);
+                editFilter.putExtra(EditFilterActivity.ARG_ID, filterResult.getId());
+                editFilter.putExtra(EditFilterActivity.ARG_NAME, filterResult.getName());
+                editFilter.putExtra(EditFilterActivity.ARG_PARAMETER, filterResult.getParameter());
+                editFilter.putExtra(EditFilterActivity.ARG_CONDITION, filterResult.getCondition());
+                editFilter.putExtra(EditFilterActivity.ARG_FILTER_TEXT, filterResult.getFilterText());
+                editFilter.putExtra(EditFilterActivity.ARG_MOVE_TO, filterResult.isMoveTo());
+                editFilter.putExtra(EditFilterActivity.ARG_FOLDER, filterResult.getFolder());
+                editFilter.putExtra(EditFilterActivity.ARG_AS_READ, filterResult.isMarkAsRead());
+                editFilter.putExtra(EditFilterActivity.ARG_AS_STARRED, filterResult.isMarkAsStarred());
+                context.startActivity(editFilter);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return filterList.size();
+    }
+
+    public FilterResult removeAt(int position) {
+        FilterResult deletedFilter = filterList.remove(position);
+        notifyItemRemoved(position);
+        return deletedFilter;
+    }
+
+    public void restoreItem(int position, FilterResult filterResult) {
+        filterList.add(position, filterResult);
+        notifyItemInserted(position);
     }
 }
