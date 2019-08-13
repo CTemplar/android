1469603f728618a5f2b7de7a83451acabef8d51f
alexdwork2018 <alex.d.work.2018@gmail.com>
2019-08-13 15:11:01 +0300

    hint for recovery email

--- app/src/main/java/mobileapp/ctemplar/com/ctemplarapp/login/step/StepPasswordFragment.java
+++ app/src/main/java/mobileapp/ctemplar/com/ctemplarapp/login/step/StepPasswordFragment.java
@@ -4,17 +4,24 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
+import android.support.v7.widget.AppCompatCheckBox;
 import android.text.Editable;
+import android.text.Html;
+import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 import mobileapp.ctemplar.com.ctemplarapp.BaseFragment;
 import mobileapp.ctemplar.com.ctemplarapp.R;
+import mobileapp.ctemplar.com.ctemplarapp.utils.EditTextUtils;
 
 public class StepPasswordFragment extends BaseFragment {
 
@@ -28,6 +35,21 @@ public class StepPasswordFragment extends BaseFragment {
 
     @BindView(R.id.fragment_step_password_confirm_input_layout)
     TextInputLayout editConfirmLayout;
+
+    @BindView(R.id.fragment_step_password_recovery_input_layout)
+    TextInputLayout editRecoveryEmailLayout;
+
+    @BindView(R.id.fragment_step_password_recovery_input)
+    TextInputEditText editRecoveryEmail;
+
+    @BindView(R.id.fragment_step_password_recovery_check)
+    AppCompatCheckBox recoveryEmailCheckBox;
+
+    @BindView(R.id.fragment_step_password_recovery_check_text)
+    TextView recoveryEmailCheckText;
+
+    @BindView(R.id.fragment_step_password_recovery_layout)
+    ConstraintLayout recoveryLayout;
 
     @Override
     protected int getLayoutId() {
@@ -48,7 +70,6 @@ public class StepPasswordFragment extends BaseFragment {
         super.onViewCreated(view, savedInstanceState);
 
         viewModel = ViewModelProviders.of(getActivity()).get(StepRegistrationViewModel.class);
-
         setListeners();
     }
 
@@ -64,7 +85,6 @@ public class StepPasswordFragment extends BaseFragment {
 
     @OnClick({R.id.fragment_step_password_next_btn})
     public void onClickNext() {
-
         if (editChoose.length() < 7) {
             editConfirmLayout.setError(getResources().getString(R.string.error_password_small));
             return;
@@ -77,15 +97,17 @@ public class StepPasswordFragment extends BaseFragment {
             editConfirmLayout.setError(getResources().getString(R.string.error_password_not_match));
             return;
         }
-
-        // validate passwords
-        if(!TextUtils.isEmpty(editChoose.getText().toString()) &&
-                !TextUtils.isEmpty(editConfirm.getText().toString()) &&
-                TextUtils.equals(editChoose.getText().toString(), editConfirm.getText().toString())) {
-
-            viewModel.setPassword(editChoose.getText().toString());
-            viewModel.changeAction(StepRegistrationActions.ACTION_NEXT);
+        if (!recoveryEmailCheckBox.isChecked()) {
+            if (EditTextUtils.isEmailValid(editRecoveryEmail.getText().toString())) {
+                viewModel.setRecoveryEmail(editRecoveryEmail.getText().toString());
+            } else {
+                editRecoveryEmailLayout.setError(getString(R.string.error_invalid_email));
+                return;
+            }
         }
+
+        viewModel.setPassword(editChoose.getText().toString());
+        viewModel.changeAction(StepRegistrationActions.ACTION_NEXT);
     }
 
     private void setListeners() {
@@ -107,5 +129,49 @@ public class StepPasswordFragment extends BaseFragment {
         };
         editConfirm.addTextChangedListener(watcher);
         editChoose.addTextChangedListener(watcher);
+
+        final Spanned shortHint = Html.fromHtml(getString(R.string.hint_step_email_recovery_short));
+        final Spanned longHint = Html.fromHtml(getString(R.string.hint_step_email_recovery_long));
+        recoveryEmailCheckText.setText(shortHint, TextView.BufferType.SPANNABLE);
+        recoveryEmailCheckText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recoveryEmailCheckText.setText(
+                        v.isSelected() ? longHint : shortHint,
+                        TextView.BufferType.SPANNABLE
+                );
+                recoveryEmailCheckText.setSelected(!v.isSelected());
+            }
+        });
+        recoveryEmailCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                showRecoveryLayout(!isChecked);
+            }
+        });
+        editRecoveryEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                editRecoveryEmailLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private void showRecoveryLayout(boolean state) {
+        if (state) {
+            recoveryLayout.setVisibility(View.VISIBLE);
+        } else {
+            recoveryLayout.setVisibility(View.GONE);
+        }
     }
 }
