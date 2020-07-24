2b2285236ef23e6d8e391b45942c03706a72a854
alexdwork2018 <alex.d.work.2018@gmail.com>
2020-07-24 19:37:29 +0300

    optimization of message thread display

--- app/src/main/java/mobileapp/ctemplar/com/ctemplarapp/repository/UserRepository.java
+++ app/src/main/java/mobileapp/ctemplar/com/ctemplarapp/repository/UserRepository.java
@@ -207,8 +207,8 @@ public class UserRepository {
     }
 
     public void saveMailboxes(List<MailboxesResult> mailboxes) {
-        if(mailboxes != null && mailboxes.size()>0) {
-            for(int i = 0; i < mailboxes.size(); i++) {
+        if (mailboxes != null && mailboxes.size() > 0) {
+            for (int i = 0; i < mailboxes.size(); ++i) {
                 MailboxesResult result = mailboxes.get(i);
 
                 MailboxEntity entity = new MailboxEntity();
@@ -230,297 +230,334 @@ public class UserRepository {
     // Requests
     public Observable<SignInResponse> signIn(SignInRequest request) {
         return service.signIn(request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<SignUpResponse> signUp(SignUpRequest request) {
         return service.signUp(request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<Response<Void>> signOut(String platform, String deviceToken) {
         return service.signOut(platform, deviceToken)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<CheckUsernameResponse> checkUsername(CheckUsernameRequest request) {
         return service.checkUsername(request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<RecoverPasswordResponse> recoverPassword(RecoverPasswordRequest request) {
         return service.recoverPassword(request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<ResponseBody> changePassword(ChangePasswordRequest request) {
         return service.changePassword(request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<RecoverPasswordResponse> resetPassword(RecoverPasswordRequest request) {
         return service.resetPassword(request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<MessagesResponse> getMessagesList(int limit, int offset, String folder) {
         return service.getMessages(limit, offset, folder)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<MessagesResponse> getStarredMessagesList(int limit, int offset) {
         return service.getStarredMessages(limit, offset, true)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<MessagesResponse> getMessage(long id) {
         return service.getMessage(id)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<Response<Void>> deleteMessages(String messageIds) {
         return service.deleteMessages(messageIds)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<EmptyFolderResponse> emptyFolder(EmptyFolderRequest request) {
         return service.emptyFolder(request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<Response<Void>> toFolder(long id, String folder) {
         return service.toFolder(id, new MoveToFolderRequest(folder))
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<MessagesResponse> getChainMessages(long id) {
         return service.getChainMessages(id)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.computation());
     }
 
     public Observable<Response<Void>> markMessageAsRead(long id, MarkMessageAsReadRequest request) {
         return service.markMessageAsRead(id, request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<Response<Void>> markMessageIsStarred(long id, boolean starred) {
         return service.markMessageIsStarred(id, new MarkMessageIsStarredRequest(starred))
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<MyselfResponse> getMyselfInfo() {
         return service.getMyself()
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
     public Observable<MailboxesResponse> getMailboxesList(int limit, int offset) {
         return service.getMailboxes(limit, offset)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-    public Observable<MessagesResult> updateMessage(long id, SendMessageRequest request) {
+    public Single<MessagesResult> updateMessage(long id, SendMessageRequest request) {
         return service.updateMessage(id, request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public MessagesResult updateMessageSync(long id, SendMessageRequest request) {
+        return service.updateMessage(id, request)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .blockingGet();
     }
 
     public Observable<MessagesResult> sendMessage(SendMessageRequest request) {
         return service.sendMessage(request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-    public Observable<MessageAttachment> uploadAttachment(MultipartBody.Part attachment,
-                                                          long message, boolean isEncrypted) {
+    public Observable<MessageAttachment> uploadAttachment(
+            MultipartBody.Part attachment,
+            long message, boolean isEncrypted
+    ) {
         return service.uploadAttachment(attachment, message, isEncrypted)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-    public Observable<MessageAttachment> updateAttachment(long id, MultipartBody.Part attachment,
-                                                          long message, boolean isEncrypted) {
+    public Single<MessageAttachment> updateAttachment(
+            long id, MultipartBody.Part attachment,
+            long message, boolean isEncrypted
+    ) {
         return service.updateAttachment(id, attachment, message, isEncrypted)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public MessageAttachment updateAttachmentSync(
+            long id,
+            MultipartBody.Part attachment,
+            long message,
+            boolean isEncrypted
+    ) {
+        return service.updateAttachment(id, attachment, message, isEncrypted)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .blockingGet();
     }
 
     public Observable<Response<Void>> deleteAttachment(long id) {
         return service.deleteAttachment(id)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<KeyResponse> getEmailPublicKeys(PublicKeysRequest request) {
         return service.getKeys(request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<FiltersResponse> getFilterList() {
         return service.getFilterList()
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<FilterResult> createFilter(CustomFilterRequest customFilterRequest) {
         return service.createFilter(customFilterRequest)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<FilterResult> updateFilter(long id, CustomFilterRequest customFilterRequest) {
         return service.updateFilter(id, customFilterRequest)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<Response<Void>> deleteFilter(long id) {
         return service.deleteFilter(id)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<ResponseBody> deleteBlacklistContact(BlackListContact contact) {
         return service.deleteBlacklistContact(contact.id)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<ResponseBody> deleteWhitelistContact(WhiteListContact contact) {
         return service.deleteWhitelistContact(contact.id)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<BlackListResponse> getBlackListContacts() {
         return service.getBlackListContacts()
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<BlackListContact> addBlacklistContact(BlackListContact contact) {
         return service.addBlacklistContact(contact)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<WhiteListResponse> getWhiteListContacts() {
         return service.getWhiteListContacts()
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<WhiteListContact> addWhitelistContact(WhiteListContact contact) {
         return service.addWhitelistContact(contact)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-    public Observable<MailboxesResult> updateDefaultMailbox(long mailboxId,
-                                                            DefaultMailboxRequest request) {
+    public Observable<MailboxesResult> updateDefaultMailbox(
+            long mailboxId,
+            DefaultMailboxRequest request
+    ) {
         return service.updateDefaultMailbox(mailboxId, request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-    public Observable<MailboxesResult> updateEnabledMailbox(long mailboxId,
-                                                            EnabledMailboxRequest request) {
+    public Observable<MailboxesResult> updateEnabledMailbox(
+            long mailboxId,
+            EnabledMailboxRequest request
+    ) {
         return service.updateEnabledMailbox(mailboxId, request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<Response<MailboxesResult>> createMailbox(CreateMailboxRequest request) {
         return service.createMailbox(request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<DomainsResponse> getDomains() {
         return service.getDomains()
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-    public Observable<SettingsEntity> updateRecoveryEmail(long settingId,
-                                                          RecoveryEmailRequest request) {
+    public Observable<SettingsEntity> updateRecoveryEmail(
+            long settingId,
+            RecoveryEmailRequest request
+    ) {
         return service.updateRecoveryEmail(settingId, request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-    public Observable<SettingsEntity> updateSubjectEncrypted(long settingId,
-                                                             SubjectEncryptedRequest request) {
+    public Observable<SettingsEntity> updateSubjectEncrypted(
+            long settingId,
+            SubjectEncryptedRequest request
+    ) {
         return service.updateSubjectEncrypted(settingId, request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-    public Observable<SettingsEntity> updateContactsEncryption(long settingId,
-                                                               ContactsEncryptionRequest request) {
+    public Observable<SettingsEntity> updateContactsEncryption(
+            long settingId,
+            ContactsEncryptionRequest request
+    ) {
         return service.updateContactsEncryption(settingId, request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-    public Observable<SettingsEntity> updateAutoSaveEnabled(long settingId,
-                                                            AutoSaveContactEnabledRequest request) {
+    public Observable<SettingsEntity> updateAutoSaveEnabled(
+            long settingId,
+            AutoSaveContactEnabledRequest request
+    ) {
         return service.updateAutoSaveEnabled(settingId, request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
-    public Observable<SettingsEntity> updateAntiPhishingPhrase(long settingId,
-                                                               AntiPhishingPhraseRequest request) {
+    public Observable<SettingsEntity> updateAntiPhishingPhrase(
+            long settingId,
+            AntiPhishingPhraseRequest request
+    ) {
         return service.updateAntiPhishingPhrase(settingId, request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<MailboxesResult> updateSignature(long mailboxId, SignatureRequest request) {
         return service.updateSignature(mailboxId, request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<CaptchaResponse> getCaptcha() {
         return service.getCaptcha()
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<CaptchaVerifyResponse> captchaVerify(CaptchaVerifyRequest request) {
         return service.captchaVerify(request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<AddFirebaseTokenResponse> addFirebaseToken(AddFirebaseTokenRequest request) {
         return service.addFirebaseToken(request)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 
     public Observable<Response<Void>> deleteFirebaseToken(String token) {
         return service.deleteFirebaseToken(token)
-                .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
 }
