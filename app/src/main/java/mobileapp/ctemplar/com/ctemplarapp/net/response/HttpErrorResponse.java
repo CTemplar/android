1fc9d7bf0fe78914aebc31ad4aa5ebb186c1852c
alexdwork2018 <alex.d.work.2018@gmail.com>
2021-02-26 18:11:36 +0200

    error handling on folders editing

--- app/src/main/java/mobileapp/ctemplar/com/ctemplarapp/net/response/HttpErrorResponse.java
+++ app/src/main/java/mobileapp/ctemplar/com/ctemplarapp/net/response/HttpErrorResponse.java
@@ -3,16 +3,8 @@ package mobileapp.ctemplar.com.ctemplarapp.net.response;
 import com.google.gson.annotations.SerializedName;
 
 public class HttpErrorResponse {
-    @SerializedName("detail")
-    private String[] detail;
-
     @SerializedName("error")
     private ErrorResponse error;
-
-
-    public String[] getDetail() {
-        return detail;
-    }
 
     public ErrorResponse getError() {
         return error;
